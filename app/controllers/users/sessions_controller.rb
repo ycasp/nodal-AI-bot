d03f04cb7647f3d7fa5fe9381fb468c10bc0284e
Yannik Caspar <y.caspar@bluewin.ch>
2025-11-25 12:09:43 +0100

    found the error for the alerts, fixed it - not login in correctly

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,14 @@
+require "pry-byebug"
+
 class Users::SessionsController < Devise::SessionsController
 
   def create
     homepage_login = params[:user][:from_homepage] == "true"
-    self.resource = warden.authenticate(auth_options)
-
+    self.resource = resource_class.new(sign_in_params)
+    resource = warden.authenticate(auth_options)
+    binding.pry
     if resource
+      set_flash_message!(:notice, :signed_in)
       sign_in(resource_name, resource)
       redirect_to root_path
     else
