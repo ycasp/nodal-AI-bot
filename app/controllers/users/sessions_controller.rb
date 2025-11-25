5ac1d2169f675d70355446e41b64afee446699cf
Yannik Caspar <y.caspar@bluewin.ch>
2025-11-25 16:16:15 +0100

    fixed the controller for the loggin on the hompeage

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,21 +1,18 @@
-require "pry-byebug"
-
 class Users::SessionsController < Devise::SessionsController
 
   def create
     homepage_login = params[:user][:from_homepage] == "true"
+    # self.resource = resource_class.new(sign_in_params)
     self.resource = resource_class.new(sign_in_params)
     resource = warden.authenticate(auth_options)
-    binding.pry
     if resource
       set_flash_message!(:notice, :signed_in)
       sign_in(resource_name, resource)
       redirect_to root_path
     else
-      self.resource = resource_class.new(sign_in_params)
       flash[:alert] = "Wrong login credidentials!"
       if homepage_login
-        @resource = resource
+        @resource = self.resource
         @resource_name = resource_name
         @devise_mapping = Devise.mappings[:user]
         render "pages/home", status: :unprocessable_entity
