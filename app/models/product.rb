f905dacfe0fae6dd83455b2d84395941cb7be670
Pedro Cunha <pedromplcunha@gmail.com>
2025-11-25 12:15:02 +0000

    added chat and messages model and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :category, presence: true
+
+  has_many :chats, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_24_145044) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_25_121256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chats", force: :cascade do |t|
+    t.string "title"
+    t.bigint "product_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_chats_on_product_id"
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "role"
+    t.text "content"
+    t.bigint "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -39,4 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_24_145044) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chats", "products"
+  add_foreign_key "chats", "users"
+  add_foreign_key "messages", "chats"
 end
