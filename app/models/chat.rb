777f44a62ab1f9f4cfd66392c134e9e6eb508329
Yannik Caspar <y.caspar@bluewin.ch>
2025-11-26 12:53:03 +0100

    connected the messages to chat

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,7 @@
 class Chat < ApplicationRecord
   belongs_to :user
 
+  has_many :messages, dependent: :destroy
   has_many :chat_products, dependent: :destroy
   has_many :products, through: :chat_products
 end
