d6f038a771709b32061fd76f67187edd8d0da3d5
Yannik Caspar <y.caspar@bluewin.ch>
2025-11-28 13:50:21 +0100

    added icon

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,7 +20,6 @@ class MessagesController < ApplicationController
       ask_message_to_llm
 
       @message.chat.generate_title_from_first_message
-      redirect_to chat_path(@chat_current)
       respond_to do |format|
         format.turbo_stream # renders `app/views/messages/create.turbo_stream.erb`
         format.html { redirect_to chat_path(@chat_current) }
@@ -28,7 +27,6 @@ class MessagesController < ApplicationController
 
     else
       set_chat_sidebar_attributes
-      render "chats/show", status: :unprocessable_entity
       respond_to do |format|
         format.turbo_stream { render turbo_stream: turbo_stream.replace("new_message", partial: "messages/form", locals: { chat: @chat_current, message: @message }) }
         format.html { render "chats/show", status: :unprocessable_entity }
